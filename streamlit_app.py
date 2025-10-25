e5244b6ba0652f6e192989367e9b2de20c2d1f11
Hamziboi <112502468+Hamziboi@users.noreply.github.com>
2025-10-25 14:33:11 +0400

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,25 +93,25 @@ with st.container():
     st.markdown("---")
     
     # Overview sections
-    with st.container():
-        st.markdown(
+  with st.container():
+    st.markdown(
         """
         <div style='
             background: linear-gradient(90deg, #A6D8FF, #D5B8FF);
             border: none;
             border-radius: 12px;
-            padding: 25px;
-            text-align: center;
+            padding: 15px;
+            text-align: left;
             color: white;
             font-weight: 600;
             box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
         '>
-            <h1 style='
+            <h3 style='
                 margin: 0;
                 text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
             '>
                 Platform overview
-            </h1>
+            </h3>
         </div>
         """,
         unsafe_allow_html=True
